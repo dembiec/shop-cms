031783f52208da8facfcbc31d8898d6997a7ff77
Dominik Rembis <dominik.rembis@gmail.com>
2021-03-04 12:51:26 +0100

    First commit
    - The Laravel project has been created
    - React installed and configured in typescript
    - Tailwind implemented

---
+++ resources/js/App.tsx
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+class App extends Component
+{
+    render() {
+        return (
+            <Router>
+                <Switch>
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default App;
